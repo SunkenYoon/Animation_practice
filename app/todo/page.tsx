80abc77f9183f531bfd53c045a286cdd110d50b3
sunkenyoon <myysk0529@gmail.com>
2024-11-24 13:45:03 +0900

    skeleton pagelist, scrollview

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,9 +1,14 @@
-import styles from "../page.module.css";
+import styles from "./page.module.css";
+import PageSection from "./components/PageSection";
+import PageList from "./components/PageList";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <PageSection />
+        <PageList />
+      </main>
     </div>
   );
 }
